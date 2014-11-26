7f0bd3247c7a9960226afc8a00418471f9f93b5c
Robson Vargas Farias <irobson@users.noreply.github.com>
2014-11-26 15:13:37 -0200

    using firefox to dont need to use driver local ;P
    using firefox to dont need to use driver local ;P

--- src/test/resources/GebConfig.groovy
+++ src/test/resources/GebConfig.groovy
@@ -9,9 +9,9 @@ import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.firefox.FirefoxDriver
 
 driver = {
-   System.setProperty('webdriver.chrome.driver', 'D:/development/drivers_at/chromedriver_win32/chromedriver.exe')
-   new ChromeDriver()
-  //new FirefoxDriver()
+  // System.setProperty('webdriver.chrome.driver', 'D:/development/drivers_at/chromedriver_win32/chromedriver.exe')
+  // new ChromeDriver()
+  new FirefoxDriver()
 }
 
 //baseUrl = "http://localhost:1200"
